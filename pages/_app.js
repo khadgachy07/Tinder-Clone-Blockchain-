53f472e575fcca056683a955e2a1b24704cc3622
khadgachy07 <khadgachy07@gmail.com>
2022-05-06 16:31:39 +0545

    Complete Tinder Clone with blockchain

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
+import { MoralisProvider } from 'react-moralis'
+import { TinderProvider } from '../context/TinderContext'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider
+      serverUrl='https://hv28qujmlfe7.usemoralis.com:2053/server'
+      appId='idMOpKwOzXtUzaVWvXNhPWsZm8Y4UGalvdjOvQsE'
+    >
+      <TinderProvider>
+        <Component {...pageProps} />
+      </TinderProvider>
+    </MoralisProvider>
+  )
 }
 
 export default MyApp
